2deb36ce80a92ece45d59ae0d0325e1751279d78
Крамар Кирилл <kk@kode.ru>
2021-08-07 13:42:24 +0200

    Initial Commit

---
+++ OwnFramefork/OwnFramefork.h
@@ -0,0 +1,18 @@
+//
+//  OwnFramefork.h
+//  OwnFramefork
+//
+//  Created by Кирилл Крамар on 07.08.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OwnFramefork.
+FOUNDATION_EXPORT double OwnFrameforkVersionNumber;
+
+//! Project version string for OwnFramefork.
+FOUNDATION_EXPORT const unsigned char OwnFrameforkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OwnFramefork/PublicHeader.h>
+
+
